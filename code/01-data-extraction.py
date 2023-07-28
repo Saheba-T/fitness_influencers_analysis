5a7a91117eed90a8e9c86184f9b7ef1954c7862b
Saheba-T <saheba.tegally@hotmail.com>
2023-07-27 17:05:27 -0700

    Polished the code and created some new features

--- code/01-data-extraction.py
+++ code/01-data-extraction.py
@@ -2,7 +2,8 @@
 
 import os
 import pandas as pd
-import youtube_scraping_functions as ytfuns  # Module to scrape channels and videos
+# Module to scrape channels and videos
+import youtube_scraping_functions as ytfuns
 
 from os.path import dirname, abspath
 from googleapiclient.discovery import build  # Google API
@@ -14,12 +15,12 @@ from functools import partial                # Use with Map to fix an argument
 api_key = input("Please provide the API key generated from Google Console: ")
 
 # List of channels ids of the 5 fitness channel which was found from their respective youtube channel url
-channel_ids = ['UCpQ34afVgk8cRQBjSJ1xuJQ', # MadFit
-               'UCvGEK5_U-kLgO6-AMDPeTUQ', # EmiWong
-               'UCIJwWYOfsCfz6PjxbONYXSg', # Blogilates
-               'UCCgLoMYIyP0U56dEhEL1wXQ', # ChloeTing
-               'UCi0AqmA_3DGPFCu5qY0LLSg', # Rebecca-Louise
-              ]
+channel_ids = ['UCpQ34afVgk8cRQBjSJ1xuJQ',  # MadFit
+               'UCvGEK5_U-kLgO6-AMDPeTUQ',  # EmiWong
+               'UCIJwWYOfsCfz6PjxbONYXSg',  # Blogilates
+               'UCCgLoMYIyP0U56dEhEL1wXQ',  # ChloeTing
+               'UCi0AqmA_3DGPFCu5qY0LLSg',  # Rebecca-Louise
+               ]
 
 # Get credentials and create an API client
 youtube = build("youtube", "v3", developerKey=api_key)
@@ -33,18 +34,21 @@ video_df = pd.DataFrame()
 # Create a dataframe of video statistics of all videos from all the channels
 for c in channels_df['ChannelName'].unique():
     print("Getting video information from channel: " + c)
-    playlist_id = channels_df.loc[channels_df['ChannelName']== c, 'playlistID'].iloc[0]
-    
+    playlist_id = channels_df.loc[channels_df['ChannelName']
+                                  == c, 'playlistID'].iloc[0]
+
     # get list of video ids of all videos in the channel
     video_ids = ytfuns.get_video_ids(youtube, playlist_id)
-    
+
     # get video statistics of each video in the channel
     video_data = pd.DataFrame(ytfuns.get_video_stats(youtube, video_ids))
 
     # concat video data of the channel to the dataframe
-    video_df = pd.concat([video_df,video_data], ignore_index=True)
-    
-# Save dataframes as csv files 
+    video_df = pd.concat([video_df, video_data], ignore_index=True)
+
+# Save dataframes as csv files
 project_dir = project_dir = dirname(dirname(abspath("01-data-extraction.py")))
-channels_df.to_csv(project_dir + "/data/raw/fitness_channels_2023_07_11.csv", index=False)
-video_df.to_csv(project_dir + "/data/raw/fitness_videos_2023_07_11.csv", index=False)
+channels_df.to_csv(
+    project_dir + "/data/raw/fitness_channels_2023_07_11.csv", index=False)
+video_df.to_csv(
+    project_dir + "/data/raw/fitness_videos_2023_07_11.csv", index=False)
